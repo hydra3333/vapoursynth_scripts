6021a3d727aca95cc2c4ae374bb71eef24984829
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2026-05-20 19:36:51 +0930

    Add files via upload

--- vscnr2_bm3d/vscnr2_bm3d.py
+++ vscnr2_bm3d/vscnr2_bm3d.py
@@ -1,5 +1,5 @@
 """
-Replacement for CNR2 chroma denoising, using bm3dcpu for chroma denoising and bwdif for deinterlacing.
+Replacement for Vapoursynth CNR2 chroma denoising, using bm3dcpu for chroma denoising and bwdif for deinterlacing.
     - Intended for use with chroma-noisey VHS captures eg for VHS-C home movies.
     - Defaults to chroma-only denoising, with optional LUMA denoising via sigma_luma.
     - Handles both progressive and interlaced (PAL/NTSC) YUV sources.
